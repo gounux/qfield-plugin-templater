244fe28730c46b6fe1c91d6b9efd114973ec5017
Guilhem Allaman <contact@guilhemallaman.net>
2026-07-03 16:42:36 +0200

    chore: remove plugin icon from cookiecutter template
    It is more simple to setup a basic default `icon.png`

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,15 +1,9 @@
 import logging
-import sys
-from pathlib import Path
 
 
 def main():
     """Pre generation hook."""
-    # check if selected icon exists
-    plugin_icon = Path("{{ cookiecutter.plugin_icon }}")
-    if not plugin_icon.is_file():
-        logging.info("Selected plugin icon doesn't exist.")
-        sys.exit(1)
+    logging.info("Running pre generation hook...")
 
 
 if __name__ == "__main__":
