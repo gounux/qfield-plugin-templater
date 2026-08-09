68b5465e0bb0882a14327996c4a137c6c1bb31cc
Guilhem Allaman <contact@guilhemallaman.net>
2026-08-09 07:57:42 +0200

    format: add per-file loggers

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,11 @@
 import logging
 
+logger = logging.getLogger(__name__)
+
 
 def main():
     """Pre generation hook."""
-    logging.info("Running pre generation hook...")
+    logger.info("Running pre generation hook...")
 
 
 if __name__ == "__main__":
